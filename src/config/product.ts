334eeedac07ece83cfaab2f2dac4e11afad044cb
jojunhyeong <jojh0323@naver.com>
2026-07-22 14:27:38 +0900

    chore: 씨미 인증 Worker 연결

--- src/config/product.ts
+++ src/config/product.ts
@@ -1,2 +1,2 @@
 // 씨미 Client Secret은 이 Cloudflare Worker의 암호화 저장소에만 보관합니다.
-export const AUTH_SERVICE_URL = 'https://YOUR_SUBDOMAIN.workers.dev';
+export const AUTH_SERVICE_URL = 'https://cime-pal-donation-auth.pal-donation-event.workers.dev';
